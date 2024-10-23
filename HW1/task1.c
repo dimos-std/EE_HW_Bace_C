ddab114fef467386e91c3f33a3dadff636450617
dimos-std <dimpost58@mail.ru>
2024-10-24 00:22:02 +0300

    task 1

---
+++ HW1/task1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argt, char **argv)
+{
+  printf("Hello world!\n");
+  return 0;
+}
---
+++ HW1/task3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int agrc, char **argv)
+{
+  printf("   *\n");
+  printf("  ***\n");
+  printf(" *****\n");
+  printf("*******\n");
+  printf(" HHOHH\n");
+  printf(" ZZZZZ\n");
+
+  return 0;
+}
